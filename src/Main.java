f10b2bad469969bc1b1c0e17d5926a4a6db7b79d
Marie-Christine Acin <Laure@MacBook-Pro-de-Marie-Christine-370.local>
2019-10-09 15:45:01 +0200

    Hello Xbee

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.rapplogic.xbee.api.XBee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XBee xBee = new XBee();
+		System.out.println("Hello Xbee");
+	}
+
+}
